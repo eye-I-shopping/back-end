52d6f1d42c1f149995080a9e1a3df7d260952d25
bong94688 <bong94688@gmail.com>
2023-05-31 11:31:27 +0900

    feat(spring) : 정확도 최댓값 + DB 상세정보 리턴

--- back-end/simbonggyo/src/main/java/com/example/TT/item/controller/itemcontroller10.java
+++ back-end/simbonggyo/src/main/java/com/example/TT/item/controller/itemcontroller10.java
@@ -1,35 +1,40 @@
 package com.example.TT.item.controller;
+
 import com.example.TT.item.dto.itemDto3;
-import com.example.TT.item.dto.jsonDto2;
-
+import com.example.TT.item.entity.test1;
+import com.example.TT.item.service.Test1Service;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 public class itemcontroller10 {
 
-    @PostMapping(value = "/api/test2", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<String>> handleJSONData(@RequestBody(required = false) itemDto3[] itemsList) {
-        // Check if itemsList is not null or empty
+    private final Test1Service test1Service;
 
-        List<String> return2 = new ArrayList<String>();
+    public itemcontroller10(Test1Service test1Service) {
+        this.test1Service = test1Service;
+    }
+
+    @PostMapping(value = "/api/test2", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> handleJSONData(@RequestBody(required = false) itemDto3[] itemsList) {
+        // Check if itemsList is not null or empty
         if (itemsList == null || itemsList.length == 0) {
-            return new ResponseEntity<>(return2, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
+        // Initialize maxConfidence and maxConfidenceItem
+        double maxConfidence = 0.0;
+        itemDto3 maxConfidenceItem = null;
+
         // Iterate through all items and log their values
-      for (itemDto3 item : itemsList) {
+        for (itemDto3 item : itemsList) {
             log.info("Item Info:");
             log.info("xmin: {}", item.getXmin());
             log.info("ymin: {}", item.getYmin());
@@ -38,10 +43,27 @@ public class itemcontroller10 {
             log.info("confidence: {}", item.getConfidence());
             log.info("class: {}", item.getClazz());
             log.info("name: {}", item.getName());
-            return2.add(item.getName());
             log.info("---");
+
+            // Check if this item's confidence is greater than maxConfidence
+            if(item.getConfidence() > maxConfidence){
+                maxConfidence = item.getConfidence();
+                maxConfidenceItem = item;
+            }
         }
-        
-        return new ResponseEntity<>(return2, HttpStatus.OK);
+
+        // If no item found with confidence more than 0, return bad request
+        if(maxConfidenceItem == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // Lookup the test1 item in the database
+        Optional<test1> dbItemOpt = test1Service.findByName(maxConfidenceItem.getName());
+        if(dbItemOpt.isEmpty()){
+            // If the item's detail is not in the database, return the item's name
+            return new ResponseEntity<>(maxConfidenceItem.getName(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(dbItemOpt.get(), HttpStatus.OK);
     }
 }
