00a8207c21b27040092ab731e87f0649d7a3c6ac
jimin <seongjimin199@gmail.com>
2023-06-02 17:41:30 +0900

    feat(spring): gpt 구현중

--- back-end/simbonggyo/src/main/java/com/example/TT/item/controller/itemcontroller11.java
+++ back-end/simbonggyo/src/main/java/com/example/TT/item/controller/itemcontroller11.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 @Slf4j
 @RestController
@@ -82,33 +85,164 @@ public class itemcontroller11 {
         // Gson 객체 생성 시 setPrettyPrinting()을 호출하여 pretty 형태로 출력하도록 설정
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        // 주어진 필드를 사용하여 GPT-3 프롬프트를 생성합니다.
-        String input = String.format("Please generate a sentence using the following details: " +
-                        "Name: %s, Category: %s, Item Detail: %s, Allegori: %s, Shape: %s, Make: %s",
+        // GPT3Prompt 객체 생성
+        GPT3Prompt prompt = new GPT3Prompt();
+        prompt.setModel("text-davinci-003");
+        prompt.setPrompt(String.format("Please generate a sentence using the following details: " +
+                "item_nm: %s, categori: %s, item_detail: %s, allegori: %s, shape: %s, make: %s",
                 test1.getName(),
                 test1.getCategori(),
                 test1.getItemDetail(),
                 test1.getAllegori(),
                 test1.getShape(),
                 test1.getMake()
-        );
+        ));
+        prompt.setMax_tokens(100);
+        prompt.setTemperature(0.0);
+        prompt.setTop_p(1.0);
+        prompt.setN(1);
+        prompt.setStream(false);
+        prompt.setLogprobs(null);
+        prompt.setStop("\n");
 
-        Gson gson2 = new Gson();
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("input", input);
-        String jsonInput = gson2.toJson(jsonObject);
+        // GPT3Prompt 객체를 JSON 문자열로 변환
+        
+        String jsonInput="";
+		try {
+			jsonInput = new String(gson.toJson(prompt).getBytes("UTF-8"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("jsonInput: " + jsonInput);
+
         try {
             // 입력을 GPT-3에 전송하고 응답을 받습니다.
             String responseJson = gpt3Service.processRequest(jsonInput);
+            System.out.println("responseJson: " + responseJson);
 
-            // 간단하게 pretty 출력으로 변환하기 위해 JsonObject로 파싱 후 반환
-            JsonObject responseObj = gson.fromJson(responseJson, JsonObject.class);
-            return gson.toJson(responseObj);
+            // 응답을 JsonObject로 파싱합니다.
+            JsonObject responseObj = new Gson().fromJson(responseJson, JsonObject.class);
+            System.out.println("responseObj: " + responseObj);
+            
+            // choices[0].message.content를 가져옵니다.
+            if (responseObj.has("choices") && responseObj.get("choices").isJsonArray()) {
+                JsonArray choicesArray = responseObj.getAsJsonArray("choices");
+                if (!choicesArray.isJsonNull() && choicesArray.size() > 0) {
+                    JsonObject choiceObject = choicesArray.get(0).getAsJsonObject();
+                    if (choiceObject.has("message") && choiceObject.get("message").isJsonObject()) {
+                        JsonObject messageObject = choiceObject.getAsJsonObject("message");
+                        if (messageObject.has("content")) {
+                            String generatedText = messageObject.get("content").getAsString();
+                            return generatedText;
+                        }
+                    }
+                }
+            }
+
+            // choices 키나 중첩된 JSON 객체/배열이 존재하지 않는 경우 null을 반환하거나 추가적인 에러 핸들링을 수행할 수 있습니다.
+            return "Error ";
 
         } catch (IOException e) {
             // API 요청이 실패할 때 예외 처리를 합니다.
-            return input;
+            String noinput = String.format("이 제품의 이름" +
+            		  "item_nm: %s, categori: %s, item_detail: %s, allegori: %s, shape: %s, make: %s",
+                    test1.getName(),
+                    test1.getCategori(),
+                    test1.getItemDetail(),
+                    test1.getAllegori(),
+                    test1.getShape(),
+                    test1.getMake());
+            return noinput;
+
+        } catch (JsonSyntaxException e) {
+            // JSON 처리 중에 문제가 발생했을 때의 예외 처리
+            log.error("Error processing GPT-3 response", e);
+            return "Error processing GPT-3 response";
         }
     }
 
+    class GPT3Prompt {
+        String model;
+        String prompt;
+        int max_tokens;
+        double temperature;
+        double top_p;
+        int n;
+        boolean stream;
+        String logprobs;
+        String stop;
+
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getPrompt() {
+            return prompt;
+        }
+
+        public void setPrompt(String prompt) {
+            this.prompt = prompt;
+        }
+
+        public int getMax_tokens() {
+            return max_tokens;
+        }
+
+        public void setMax_tokens(int max_tokens) {
+            this.max_tokens = max_tokens;
+        }
+
+        public double getTemperature() {
+            return temperature;
+        }
+
+        public void setTemperature(double temperature) {
+            this.temperature = temperature;
+        }
+
+        public double getTop_p() {
+            return top_p;
+        }
+
+        public void setTop_p(double top_p) {
+            this.top_p = top_p;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+
+        public boolean isStream() {
+            return stream;
+        }
+
+        public void setStream(boolean stream) {
+            this.stream = stream;
+        }
+
+        public String getLogprobs() {
+            return logprobs;
+        }
+
+        public void setLogprobs(String logprobs) {
+            this.logprobs = logprobs;
+        }
+
+        public String getStop() {
+            return stop;
+        }
+
+        public void setStop(String stop) {
+            this.stop = stop;
+        }
+    }
 }
