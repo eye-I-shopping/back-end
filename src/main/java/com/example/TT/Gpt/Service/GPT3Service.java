3d41e2ffa7e96da68694b370563961ea967416f3
bong94688 <bong94688@gmail.com>
2023-06-05 11:24:02 +0900

    feat(spring): jar파일 추가

--- src/main/java/com/example/TT/Gpt/Service/GPT3Service.java
+++ src/main/java/com/example/TT/Gpt/Service/GPT3Service.java
@@ -16,7 +16,7 @@ import okhttp3.Response;
 
 @Service
 public class GPT3Service {
-    private final String apiKey = "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"; // OpenAI API 키를 여기에 입력해주세요
+    private final String apiKey = "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"; // OpenAI API 키를 여기에 입력해주세요
     private final OkHttpClient client = new OkHttpClient();
 
     public String processRequest(String jsonInput) throws IOException {
